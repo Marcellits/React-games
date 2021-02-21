f446e66b603a0263c4ecb4f814b2d7646777d14c
marcellits <garcia.marcella@hotmail.com>
2021-02-20 19:05:06 -0500

    adding Tictactoe1p

---
+++ src/components/TicTacToeOnePlayer.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import "../styles/TTT.css";
+
+function TicTacToeOnePlayer() {
+  const emptyBoard = ["", "", "", "", "", "", "", "", ""];
+  const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const userPlayer = (index) => {
+    //check if the game already has a winner
+    if (winner) return null;
+    //check if the board is already clicked before
+    if (board[index] !== "") return null;
+
+    setBoard(
+      //bringing the previous clicks results each time current
+      //player clicks in a new board
+      board.map((item, itemIndex) =>
+        itemIndex === index ? currentPlayer : item
+      )
+    );
+
+    //toggle between players
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const robotPlayer = (index) => {
+      index = Math.floor(Math.random() * board.length);
+      //check if the game already has a winner
+      if (winner) return null;
+      //check if the board is already clicked before
+      if (board[index] !== "") return null;
+
+    setBoard(
+      //bringing the previous clicks results each time current
+      //player clicks in a new board
+      board.map((item, itemIndex) =>
+        itemIndex === index ? currentPlayer : item
+      )
+    )
+     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const checkWinner = () => {
+    //8 ways to win this game:
+    const waysToWin = [
+      //horizontal
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      //vertical
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      //diagonal
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    //for each cell verify if cell is O or X, using every to check array.
+    waysToWin.forEach((cells) => {
+      if (cells.every((cell) => cell === "O")) setWinner("O");
+      if (cells.every((cell) => cell === "X")) setWinner("X");
+    });
+  };
+
+  const checkDraw = () => {
+    if (board.every((cell) => cell !== "") && winner === null) {
+      setWinner("D");
+    }
+  };
+  checkDraw();
+
+  //only call winner when state has changed
+  useEffect(checkWinner, [board]);
+
+  const resetGame = () => {
+    setCurrentPlayer("X");
+    setBoard(emptyBoard);
+    setWinner(null);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1 className="title">Tic Tac Toe</h1>
+        <div class={`board ${winner ? "game-over" : ""}`}>
+          {board.map((item, index) => (
+            <div
+              key={index}
+              className={`cell ${item}`}
+              onClick={() => userPlayer(index)}
+            >
+              {winner ? "" : currentPlayer === "O" ? robotPlayer(index): ""}
+              {item}
+            </div>
+          ))}
+        </div>
+        {winner && (
+          <div className="footer">
+            {winner === "D" ? (
+              <h2 className="winner-message">
+                <span className={winner}>Draw!</span>
+              </h2>
+            ) : (
+              <h2 className="winner-message">
+                <span className={winner}>{winner}</span> Won!
+              </h2>
+            )}
+
+            <button onClick={resetGame}>Play Again!</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default TicTacToeOnePlayer;
